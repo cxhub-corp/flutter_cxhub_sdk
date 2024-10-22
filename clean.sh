506cbadd74a81ef70b834fdf52c6c5a66baab093
Алексей Ерофеев <a.erofeev@vk.team>
2024-10-22 16:25:18 +0300

    update podspec

--- clean.sh
+++ clean.sh
@@ -1,23 +1,17 @@
 cd example
 flutter clean
 cd ../cxhub_platform_interface
-flutter pub get
+flutter clean
 cd ../cxhub_android_firebase
 flutter clean
-flutter pub get
 cd ../cxhub_android_huawei
 flutter clean
-flutter pub get
 cd ../cxhub_android_rustore
 flutter clean
-flutter pub get
 cd ../cxhub_ios
 flutter clean
-flutter pub get
 cd ../cxhub_sdk
 flutter clean
-flutter pub get
 cd ../example
-flutter pub get
 cd ..
 
