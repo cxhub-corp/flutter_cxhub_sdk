fe270faafd4933eb62dd188ae2c78610bf48a205
Alexey Erofeev <erofeev1982@gmail.com>
2024-11-07 16:44:48 +0300

    - remove common project  - add common implementation in android platform modules  - add switching platform modules by version name

--- cxhub_android_firebase/android/src/main/kotlin/plugin/cxhub_sdk/CxHubSdkPluginFirebase.kt
+++ cxhub_android_firebase/android/src/main/kotlin/plugin/cxhub_sdk/CxHubSdkPluginFirebase.kt
@@ -2,7 +2,7 @@ package plugin.cxhub_sdk
 
 import cxhub.api.FirebasePlatformManager
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import plugin.common.CxHubSdkPluginCommon
+import plugin.cxhub_sdk.common.CxHubSdkPluginCommon
 
 /** CxhubSdkPlugin */
 class CxHubSdkPluginFirebase : FlutterPlugin {
