f99362df81ecc5e744e0662c1f56a2a74eb6ae80
vladimir.kukhar <vladimir.kukhar@vk.team>
2024-12-11 16:41:27 +0300

    CXT-8432: working ContentExtension

---
+++ example/ios/ContentExtension/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  ContentExtension
+//
+//  Created by Vladimir Kukhar on 09.12.2024.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
